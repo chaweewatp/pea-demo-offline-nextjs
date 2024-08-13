69a78e641633d6ff7400761ab352e029216aa4c7
pornchai <chaweewat.p@gmail.com>
2024-08-13 13:22:33 +0700

    x

--- app/page.js
+++ app/page.js
@@ -11,89 +11,59 @@ const tasks = [
 ];
 
 export default function TaskList() {
-  if (typeof window !== "undefined") {
-    // Ensure this code runs only on the client side
+  const sendData = async () => {
 
-    const [isOnline, setIsOnline] = useState(navigator.onLine);
+    try {
+      const taskData = tasks.map((task) => {
+        const images =
+          JSON.parse(localStorage.getItem(`task_${task.id}_images`)) || [];
+        return { id: task.id, name: task.name, images };
+      });
 
-    useEffect(() => {
-      const updateOnlineStatus = () => {
-        console.log("setIsOnline:");
-        console.log(navigator.onLine);
-        setIsOnline(navigator.onLine);
-      };
+      const response = await fetch("/api/sendData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(taskData),
+      });
 
-      window.addEventListener("online", updateOnlineStatus);
-      window.addEventListener("offline", updateOnlineStatus);
-
-      updateOnlineStatus();
-
-      return () => {
-        window.removeEventListener("online", updateOnlineStatus);
-        window.removeEventListener("offline", updateOnlineStatus);
-      };
-    }, []);
-
-    const sendData = async () => {
-      if (!isOnline) {
-        alert("No internet connection. Please try again later.");
-        return;
-      }
-
-      try {
-        const taskData = tasks.map((task) => {
-          const images =
-            JSON.parse(localStorage.getItem(`task_${task.id}_images`)) || [];
-          return { id: task.id, name: task.name, images };
+      if (response.ok) {
+        tasks.forEach((task) => {
+          localStorage.removeItem(`task_${task.id}_images`);
         });
-
-        const response = await fetch("/api/sendData", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(taskData),
-        });
-
-        if (response.ok) {
-          tasks.forEach((task) => {
-            localStorage.removeItem(`task_${task.id}_images`);
-          });
-          alert("Data sent successfully and tasks deleted.");
-        } else {
-          alert("Failed to send data. Please try again.");
-        }
-      } catch (error) {
-        console.error("Error sending data:", error);
-        alert("An error occurred. Please try again later.");
+        alert("Data sent successfully and tasks deleted.");
+      } else {
+        alert("Failed to send data. Please try again.");
       }
-    };
+    } catch (error) {
+      console.error("Error sending data:", error);
+      alert("An error occurred. Please try again later.");
+    }
+  };
 
-    return (
-      <div className="container mx-auto mt-4">
-        <h1 className="text-2xl font-bold mb-4">Task List</h1>
-        <ul className="list-disc pl-5">
-          {tasks.map((task) => (
-            <li key={task.id} className="mb-2">
-              <Link
-                className="text-blue-600 hover:underline"
-                href={`/tasks/${task.id}`}
-              >
-                {task.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <button
-          onClick={sendData}
-          className={`mt-4 px-4 py-2 bg-blue-500 text-white rounded ${
-            !isOnline ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-700"
-          }`}
-          disabled={!isOnline}
-        >
-          Send All Data
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="container mx-auto mt-4">
+      <h1 className="text-2xl font-bold mb-4">Task List</h1>
+      <ul className="list-disc pl-5">
+        {tasks.map((task) => (
+          <li key={task.id} className="mb-2">
+            <Link
+              className="text-blue-600 hover:underline"
+              href={`/tasks/${task.id}`}
+            >
+              {task.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={sendData}
+        className={`mt-4 px-4 py-2 bg-blue-500 text-white rounded `}
+      >
+        Send All Data
+      </button>
+    </div>
+  );
 }
+
--- app/tasks/[id]/page.js
+++ app/tasks/[id]/page.js
@@ -3,11 +3,8 @@
 
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
-import dynamic from 'next/dynamic';
-
-const ImageUploader = dynamic(() => import('../../../components/ImageUploader'), { ssr: false });
-const ImageList = dynamic(() => import('../../../components/ImageList'), { ssr: false });
-
+import ImageUploader from '../../../components/ImageUploader';
+import ImageList from '../../../components/ImageList';
 
 export default function TaskPage() {
   const { id } = useParams(); // Use `useParams` to get the task ID
@@ -16,52 +13,17 @@ export default function TaskPage() {
   const [isOnline, setIsOnline] = useState(true);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const updateOnlineStatus = () => {
-      console.log("setIsOnline");
-      console.log(navigator.onLine)
-      setIsOnline(navigator.onLine);
-    };
-
-    window.addEventListener('online', updateOnlineStatus);
-    window.addEventListener('offline', updateOnlineStatus);
-
-    updateOnlineStatus();
-
-    return () => {
-      window.removeEventListener('online', updateOnlineStatus);
-      window.removeEventListener('offline', updateOnlineStatus);
-    };
-  }, []);
 
   useEffect(() => {
     if (id) {
-      if (isOnline) {
-        console.log('do something')
-        // fetch(`/api/tasks/${id}`)
-        //   .then((response) => response.json())
-        //   .then((data) => {
-        //     setTask(data.task || { name: '' });
-        //     const existingImages = JSON.parse(localStorage.getItem(`task_${id}_images`)) || [];
-        //     const fetchedImages = data.images || [];
-        //     const updatedImages = [...existingImages, ...fetchedImages];
-        //     setImageList(updatedImages);
-        //     localStorage.setItem(`task_${id}_images`, JSON.stringify(updatedImages));
-        //     setIsLoading(false);
-        //   })
-        //   .catch((error) => {
-        //     console.error('Error fetching task data:', error);
-        //     setIsLoading(false);
-        //   });
-      } else {
         const storedTask = JSON.parse(localStorage.getItem(`task_${id}`)) || { name: '' };
         const storedImages = JSON.parse(localStorage.getItem(`task_${id}_images`)) || [];
         setTask(storedTask);
         setImageList(storedImages);
         setIsLoading(false);
-      }
+    
     }
-  }, [id, isOnline]);
+  }, [id]);
 
   const addImage = (newImage) => {
     const updatedImages = [...imageList, newImage];
